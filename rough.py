86f6470ae302405d22d8e680597888663ba1d4b8
b4batunde <devamritapython@gmail.com>
2025-05-22 09:43:58 +0530

    Rough Code

--- rough.py
+++ rough.py
@@ -1,6 +1,24 @@
-import numpy as np
+import numpy
 
-listx = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
-n = np.arange(10)
-n = n.reshape(2, 5)
-print(n)
+
+items = numpy.array([
+    "Truffle Risotto",
+    "Foie Gras",
+    "Caviar Blinis",
+    "Wagyu Beef",
+    "Lobster Thermidor",
+    "Duck à l'Orange",
+    "Coq au Vin",
+    "Oysters Rockefeller",
+    "Beef Wellington",
+    "Saffron Paella"])
+
+nums = numpy.random.default_rng(2022)
+prices = nums.normal(25, 5, 10)
+
+fItems = items[prices > 25]
+print(fItems)
+
+
+final = numpy.where(prices > 20, 0, 5)
+print(final)
